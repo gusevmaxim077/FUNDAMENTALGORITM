a6e95c249e109476ee77e35c08098fb9fee912fa
gusevmaxim077 <maksim08082005@mail.ru>
2025-04-09 18:55:12 +0000

    я устал

--- 4A_grafves.py
+++ 4A_grafves.py
@@ -1,3 +1,5 @@
+import timeit
+
 def count_vertices(edges):
     """Подсчет количества вершин в графе."""
     vertices = set()
@@ -19,9 +21,8 @@ def get_vertices(edges):
 
 # Пример использования
 edges = [(1, 2), (2, 3), (3, 4), (4, 5), (1, 6), (6, 7), (7, 5)]
-
 vertices = get_vertices(edges)
-my_dict = {1: 777,2: 666,3: 555,4: 444,5: 333,6: 222,7: 111}
+my_dict = {'1': 777,'2': 666,'3': 555,'4': 444,'5': 333,'6': 222,'7': 111}
 
 #n = len(my_dict)
 #for i in range(n):
@@ -31,12 +32,44 @@ my_dict = {1: 777,2: 666,3: 555,4: 444,5: 333,6: 222,7: 111}
 #            my_dict[1] = my_dict[2]
 #            my_dict[2] = fff
 #print(my_dict)
+start = timeit.default_timer()
+print("The start time is :",start)
+n = len(my_dict)
+spis = list(my_dict.items())
+for i in range(n):
+    for j in range(0,n-i-1):
+        if spis[j][1] > spis[j+1][1]:
+            spis[j], spis[j + 1] = spis[j+1], spis[j]
+print("The difference of time is :", timeit.default_timer() - start)
+print(spis)
 
-my_dictred = my_dict
-for i in my_dict:
-    my_dictred = my_dictred.popitem()
-    for j in my_dictred:
-        if my_dictred[j] > my_dictred[j+1]
+### for i in my_dict: ключи
+
+
+
+value = int(input())
+
+    
+
+# индексы первого элемента, последнего и среднего
+low = 0
+high = len(spis) - 1
+mid = len(spis) // 2
+    
+
+while spis[mid][1] != value and low <= high:
+    if value > spis[mid][1]:
+        low = mid + 1
+    else:
+        high = mid - 1
+    mid = (low + high) // 2
+        
+
+if low > high:
+    print('no element')
+else:
+    print("id:",spis[mid][0])
+
 
 
 
